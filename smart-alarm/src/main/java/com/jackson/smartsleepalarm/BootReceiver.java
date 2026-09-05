2387beeaca46d39f2d9f3ba336fc7fa707a279fb
jacksonjia666 <jacksonjia666@gmail.com>
2026-09-06 00:33:37 +1000

    Keep smart alarm alive: BootReceiver.java

--- smart-alarm/src/main/java/com/jackson/smartsleepalarm/BootReceiver.java
+++ smart-alarm/src/main/java/com/jackson/smartsleepalarm/BootReceiver.java
@@ -1,5 +1,5 @@
 package com.jackson.smartsleepalarm;
 import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;
 public final class BootReceiver extends BroadcastReceiver {
-    @Override public void onReceive(Context c,Intent i){if(Scheduler.enabled(c))Scheduler.scheduleNextWindow(c);}
+    @Override public void onReceive(Context c,Intent i){if(Scheduler.enabled(c)){Scheduler.scheduleNextWindow(c);Scheduler.startMonitorService(c);}}
 }
