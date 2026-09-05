1b6cb6c50d241f9154f380e23e7d4dae5abddc13
jacksonjia666 <jacksonjia666@gmail.com>
2026-09-06 02:18:30 +1000

    Release Screen Insight 2.0: build.gradle.kts

--- screen-insight/build.gradle.kts
+++ screen-insight/build.gradle.kts
@@ -2,6 +2,6 @@ plugins { id("com.android.application") }
 android {
     namespace = "com.jackson.screeninsight"
     compileSdk = 35
-    defaultConfig { applicationId = "com.jackson.screeninsight"; minSdk = 28; targetSdk = 35; versionCode = 5; versionName = "1.4" }
+    defaultConfig { applicationId = "com.jackson.screeninsight"; minSdk = 28; targetSdk = 35; versionCode = 20; versionName = "2.0" }
     buildTypes { release { isMinifyEnabled = true; isShrinkResources = true; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro") } }
 }
