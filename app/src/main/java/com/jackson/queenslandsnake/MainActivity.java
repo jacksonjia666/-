2254143852630c706bfbdf14d7a14e2cf3c13bc6
jacksonjia666 <jacksonjia666@gmail.com>
2026-09-05 16:33:44 +1000

    Add app/src/main/java/com/jackson/queenslandsnake/MainActivity.java

---
+++ app/src/main/java/com/jackson/queenslandsnake/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jackson.queenslandsnake;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public final class MainActivity extends Activity {
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new SnakeView(this));
+    }
+}
