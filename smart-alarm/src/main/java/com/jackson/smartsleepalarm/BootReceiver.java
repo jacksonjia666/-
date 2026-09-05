7d40777ca05989c1d87c0875e1df09b896b3d9b4
jacksonjia666 <jacksonjia666@gmail.com>
2026-09-05 23:54:23 +1000

    Add smart alarm: smart-alarm/src/main/java/com/jackson/smartsleepalarm/BootReceiver.java

---
+++ smart-alarm/src/main/java/com/jackson/smartsleepalarm/BootReceiver.java
@@ -0,0 +1,5 @@
+package com.jackson.smartsleepalarm;
+import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;
+public final class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context c,Intent i){if(Scheduler.enabled(c))Scheduler.scheduleNextWindow(c);}
+}
