415013a19c6144e939e8f1088ce117ee7b0466a9
jacksonjia666 <jacksonjia666@gmail.com>
2026-09-06 01:00:51 +1000

    Add Screen Insight: screen-insight/src/main/java/com/jackson/screeninsight/BootReceiver.java

---
+++ screen-insight/src/main/java/com/jackson/screeninsight/BootReceiver.java
@@ -0,0 +1,3 @@
+package com.jackson.screeninsight;
+import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;
+public final class BootReceiver extends BroadcastReceiver {@Override public void onReceive(Context c,Intent i){ReportScheduler.schedule(c);}}
